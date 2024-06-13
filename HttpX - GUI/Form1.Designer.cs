5e93917548ac40dae279f043aab287a77e921b36
Mf4Tn <75338199+Mf4Tn@users.noreply.github.com>
2024-06-13 01:16:03 +0100

    Add files via upload

---
+++ HttpX - GUI/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace HttpX___GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Import Domains";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(118, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total Domains: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(206, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(16, 52);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(461, 386);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Domain";
+            this.columnHeader1.Width = 214;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Response Status code";
+            this.columnHeader2.Width = 241;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(377, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(489, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HttpX - GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ HttpX - GUI/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Leaf.xNet;
+using System.Threading;
+
+namespace HttpX___GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public List<string> domains = new List<string>();
+        int total_domains = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] text = Clipboard.GetText().Split('\n');
+            foreach(var line in text)
+            {
+                domains.Add(line);
+
+            }
+            total_domains = domains.Count;
+            label2.Text = total_domains.ToString();
+            MessageBox.Show($"Total Domains Saved (From clipboard): '{total_domains.ToString()}' .", "Done", MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach(var domain in domains)
+            {
+                new Thread(() =>
+                {
+                    try
+                    {
+                        var d = domain;
+                        if (!domain.Contains("://"))
+                        {
+                            d = "http://" + domain;
+                        }
+                        int status_code = (int)new HttpRequest().Get(d).StatusCode;
+                        ListViewItem item = new ListViewItem(domain);
+                        item.SubItems.Add(status_code.ToString());
+                        listView1.Invoke(new MethodInvoker(delegate
+                        {
+                            listView1.Items.Add(item);
+                        }));
+                    }
+                    catch (HttpException ex)
+                    {
+                        int stats = 0;
+                        if(ex.HttpStatusCode.ToString() == "Conflict")
+                        {
+                            stats = 409;
+                        }else if (ex.HttpStatusCode.ToString() == "InternalServerError")
+                        {
+                            stats = 500;
+                        }
+                        else if(ex.HttpStatusCode.ToString().ToLower() == "notfound")
+                        {
+                            stats = 404;
+                        }
+                        ListViewItem item = new ListViewItem(domain);
+                        if (stats != 0) item.SubItems.Add(stats.ToString());
+                        else item.SubItems.Add(ex.HttpStatusCode.ToString());
+                        listView1.Invoke(new MethodInvoker(delegate
+                        {
+                            listView1.Items.Add(item);
+                        }));
+
+                    }
+                    catch
+                    {
+
+                    }
+                })
+                { IsBackground = true }.Start();
+
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            string t = "";
+            foreach(ListViewItem item in listView1.SelectedItems)
+            {
+                t += item.SubItems[0].Text + '\n';
+            }
+            Clipboard.SetText(t);
+            MessageBox.Show("Copied to Clipboard !", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
